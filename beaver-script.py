3bf702479ce54c1e280fe2c8c768a5abbc93751d
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-09-20 11:06:54 +0200

    Create beaver-script.py

---
+++ beaver-script.py
@@ -0,0 +1,12 @@
+class Lokalita:
+  def __init__(self):
+    self.beavers = []
+
+  def __str__(self):
+    return "Test"
+
+  def load(self, filePath):
+    return "Test"
+
+lokalit = Lokalita()
+print(lokalit)

62ecac51e985f141edc71cbff7ce46ae8aba2746
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-09-20 13:43:08 +0200

    Update beaver-script.py

--- beaver-script.py
+++ beaver-script.py
@@ -1,23 +1,34 @@
 import json
+import random
 
 class Lokalita:
-  def __init__(self):
-    self.beavers = []
-    self.dens = []
+    def __init__(self):
+        self.beavers = []
+        self.dens = []
 
-  def __str__(self):
-    strin = ""
-    for i in self.beavers:
-        strin += f"Beaver {i['id']} lives in Den {i['den_id']}. "
-    return strin
+    def __str__(self):
+        strin = ""
+        for i in self.beavers:
+            strin += f"Beaver {i['id']}:\n   is {i['age']} years old\n   is {i['sex']}\n"
+            if i["den_id"]:
+                strin += f"   lives in den {i['den_id']}\n"
+            else:
+                strin += f"   is homeless\n"
+            strin += "\n"
+        return strin
 
-  def load(self, filePath, typ):
-    if typ == "beaver":
-      self.beavers = json.loads(open(filePath, "r"))
-    elif typ == "den":
-      self.dens = json.loads(open(filePath, "r"))
+    def load(self, filePath, typ):
+        if typ == "beaver":
+            self.beavers = json.loads(open("beaver-list.json", "r").read())
+        elif typ == "den":
+            self.dens = json.loads(open(filePath, "r").read())
+
+    def assign(self):
+        for beaver in self.beavers:
+            beaver["den_id"] = random.choice(self.dens)["id"]
 
 lokalit = Lokalita()
-lokalit.load(".\beaver-list.json", "beaver")
-lokalit.load(".\dens-list.json", "den")
+lokalit.load("beaver-list.json", "beaver")
+lokalit.load("dens-list.json", "den")
+lokalit.assign() # assign random dens to beavers
 print(lokalit)

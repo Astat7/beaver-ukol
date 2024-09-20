c00a1139a985834f263c6d6b49955011c3a9360b
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-09-20 11:18:11 +0200

    Update beaver-script.py

--- beaver-script.py
+++ beaver-script.py
@@ -3,13 +3,18 @@ import json
 class Lokalita:
   def __init__(self):
     self.beavers = []
+    self.dens = []
 
   def __str__(self):
     return "Test"
 
-  def load(self, filePath):
-    self.beavers = json.loads(open(filePath, "r"))
+  def loadBeaver(self, filePath, typ):
+    if typ == "beaver":
+      self.beavers = json.loads(open(filePath, "r"))
+    elif typ == "den":
+      self.dens = json.loads(open(filePath, "r"))
 
 lokalit = Lokalita()
-lokalit.load(".\beaver-list.json")
+lokalit.load(".\beaver-list.json", "beaver")
+lokalit.load(".\dens-list.json", "den")
 print(lokalit)
